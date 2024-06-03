4e8d234aeaa152ffc8c8b9f0450c39ce105a81c2
Maria Schulze <maria.ebs865@gmail.com>
2024-06-03 20:21:36 -0300

    pontuacao

--- main.py
+++ main.py
@@ -1,35 +1,82 @@
 import pygame
+import random
 pygame.init()
-tamanho  = ( 800, 600)
-clock = pygame.time.Clock()
-tela = pygame.display.set_mode(tamanho)
-pygame.display.set_caption("IronMan ME")
+tamanho = (800,600)
+relogio = pygame.time.Clock()
+tela = pygame.display.set_mode( tamanho ) 
+pygame.display.set_caption("Iron Man do Marcão")
 branco = (255,255,255)
-preto = (0,0,0)
-posicaoXpersona = 400
-posicaoYpersona = 300
-movimentoXpersona = 0
+preto = (0, 0 ,0 )
+iron = pygame.image.load("assets/iron.png")
+fundo = pygame.image.load("assets/fundo.png")
+missel = pygame.image.load("assets/missile.png")
+posicaoXPersona = 400
+posicaoYPersona = 300
+movimentoXPersona  = 0
+movimentoYPersona  = 0
+posicaoXMissel = 400
+posicaoYMissel = -240
+velocidadeMissel = 1
+missileSound = pygame.mixer.Sound("assets/missile.wav")
+pygame.mixer.Sound.play(missileSound)
+fonte = pygame.font.SysFont("comicsans",28)
+pygame.mixer.music.load("assets/ironsound.mp3")
+pygame.mixer.music.play(-1)
+pontos = 0
 
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             quit()
         elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_RIGHT:
-            movimentoXpersona = 5
+            movimentoXPersona = 10
         elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_LEFT:
-            movimentoXpersona = -5    
+            movimentoXPersona = -10
         elif evento.type == pygame.KEYUP and evento.key == pygame.K_RIGHT:
-            movimentoXpersona = 0
+            movimentoXPersona = 0
         elif evento.type == pygame.KEYUP and evento.key == pygame.K_LEFT:
-            movimentoXpersona = 0
-
+            movimentoXPersona = 0
+        elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_UP:
+            movimentoYPersona = -10
+        elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_DOWN:
+            movimentoYPersona = 10
+        elif evento.type == pygame.KEYUP and evento.key == pygame.K_UP:
+            movimentoYPersona = 0
+        elif evento.type == pygame.KEYUP and evento.key == pygame.K_DOWN:
+            movimentoYPersona = 0
+            
+    posicaoXPersona = posicaoXPersona + movimentoXPersona            
+    posicaoYPersona = posicaoYPersona + movimentoYPersona            
+    
+    if posicaoXPersona < 0 :
+        posicaoXPersona = 10
+    elif posicaoXPersona >550:
+        posicaoXPersona = 540
         
-    posicaoXpersona = posicaoXpersona + movimentoXpersona
-
+    if posicaoYPersona < 0 :
+        posicaoYPersona = 10
+    elif posicaoYPersona > 473:
+        posicaoYPersona = 463
+    
+        
     tela.fill(branco)
-    pygame.draw.circle(tela, preto, (posicaoXpersona,posicaoYpersona), 40, 0)
-
+    tela.blit(fundo, (0,0) )
+    #pygame.draw.circle(tela, preto, (posicaoXPersona,posicaoYPersona), 40, 0 )
+    tela.blit( iron, (posicaoXPersona, posicaoYPersona) )
+    
+    posicaoYMissel = posicaoYMissel + velocidadeMissel
+    if posicaoYMissel > 600:
+        posicaoYMissel = -240
+        pontos = pontos + 1
+        velocidadeMissel = velocidadeMissel + 1
+        posicaoXMissel = random.randint(0,800)
+        pygame.mixer.Sound.play(missileSound)
+        
+        
+    tela.blit( missel, (posicaoXMissel, posicaoYMissel) )
+    
+    texto = fonte.render("Pontos: "+str(pontos), True, branco)
+    tela.blit(texto, (10,10))
+    
     pygame.display.update()
-    clock.tick(60)
-
-pygame.quit()
+    relogio.tick(60)
